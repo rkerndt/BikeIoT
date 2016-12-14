4cecda43385611418e3733d590e4b8c0c570c710
Rickie Kerndt <kerndtr@kerndt.com>
2016-12-13 22:17:12 -0800

    wrong command

--- Beacon.py
+++ Beacon.py
@@ -111,8 +111,7 @@ def bt_process():
                 grovepi.digitalWrite(LED, data[0])
     except IOError as e:
         if DEBUG:
-            pass
-            #print('IOError detected and excepted: %s' % (str(e),))
+            print('IOError detected and excepted: %s' % (str(e),))
     except:
       message = "Unexpected error: %s" % sys.exc_into()[0]
       raise Beacon_Error(message)
